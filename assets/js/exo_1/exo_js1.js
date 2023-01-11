aaf295d6c5b88dfe22b59eb1ea0f60e705ee50a5
lleloir <leo.lelloir24@outlook.fr>
2023-01-11 16:43:56 +0100

    JS

---
+++ assets/js/exo_1/exo_js1.js
@@ -0,0 +1,18 @@
+var age = window.prompt("saisissez vôtre âge");
+var i = 0;
+var table = "a"
+while (i <= 100) 
+{   
+    // Exécute le calcul et stocke le résultat   
+    // dans une variable nommée ‘resultat’  
+    resultat = age>20;
+
+    // A chaque tour, on affiche le résultat courant à l’utilisateur
+    console.log("Personne de moins de 20 ans:"+a+);
+
+    // A chaque tour, on ajoute +1 à la variable i  
+    i++; 
+}
+
+// instructions exécutées après le for (i vaut 10)
+console.log("fin de la boucle");
