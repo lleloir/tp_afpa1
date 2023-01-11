9afab53808836ec1ca78372a213a25c6cdee76c1
lleloir <leo.lelloir24@outlook.fr>
2023-01-11 11:00:09 +0100

    suite

--- assets/js/exo_1/js_exo_1.js
+++ assets/js/exo_1/js_exo_1.js
@@ -1,57 +1,20 @@
-var bouton1= document.getElementById("Id_btn1");
-bouton1.addEventListener("click",clickbtn1);
+var i = 0;
 
-function clickbtn1(){
+console.log("Table de multiplication par 5");
+console.log("=============================");
 
-    alert("Vous avez cliqué sur :\n Alert()");
+while (i <= 10) 
+{   
+    // Exécute le calcul et stocke le résultat   
+    // dans une variable nommée ‘resultat’  
+    resultat = 5 * i;
+
+    // A chaque tour, on affiche le résultat courant à l’utilisateur
+    console.log("Le résultat de 5 x "+i+" est : "+resultat);
+
+    // A chaque tour, on ajoute +1 à la variable i  
+    i++; 
 }
 
-var bouton2= document.getElementById("Id_btn2");
-bouton2.addEventListener("click",clickbtn2);
-
-function clickbtn2(){
-
-    var prenom = prompt("Veuillez saisir votre prénom");
-    if(prenom==null){
-    alert("Vous avez cliqué sur Annuler");
-    return;
-    }
-
-    var age= parseInt(prompt("Veuillez saisir votre age"));
-    if(isNaN(age)){
-    alert("Vous n'avez pas saisi un age correct !\nVeuillez recommencer.");
-    return;
-    }  
-
-    alert("Voici ce que vous avez saisi :\n\nVotre prénom : "+prenom+"\nVotre age : " +age);
-}
-
-var bouton3= document.getElementById("Id_btn3");
-bouton3.addEventListener("click",clickbtn3);
-
-function clickbtn3(){
-
-    var reponse = confirm("Veuillez cliquer sur OK ou bien Annuler");
-    if (reponse == true) {
-        alert("Vous avez cliqué sur OK");
-    } 
-    else {
-        alert("Vous avez cliqué sur Annuler");
-    }
-
-}
-
-var bouton4= document.getElementById("Id_btn4");
-bouton4.addEventListener("click",clickbtn4);
-
-function clickbtn4(){
-
-    var prenom = prompt("Veuillez saisir votre prénom");
-    if(prenom==null){
-        alert("Vous avez cliqué sur Annuler");
-    }
-    else{
-        console.log("Voici le prénom que vous avez saisi: "+prenom);
-        alert("Vérifiez en Console (F12), ce que vous venez de saisir...");
-    }
-}
+// instructions exécutées après le for (i vaut 10)
+console.log("fin de la boucle");
