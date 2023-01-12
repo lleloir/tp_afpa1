f7e63cd3fccf7f2767c26b48f01bc43c06493e8f
lleloir <leo.lelloir24@outlook.fr>
2023-01-12 13:40:50 +0100

    modification onglet

---
+++ assets/js/exo_3/exo_js3.js
@@ -0,0 +1,7 @@
+let table=["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+let prenom= prompt("saisir un prenom");
+if(prenom==audrey){
+    alert("un prenom trouvé de la liste");
+    [audrey]=[""]
+    
+}
