d2084dc128d2157d56c500b4fa20036b568d7dda
lleloir <leo.lelloir24@outlook.fr>
2023-01-17 13:43:57 +0100

    fin exo 3

--- assets/js/exo_3/exo_js3.js
+++ assets/js/exo_3/exo_js3.js
@@ -1,10 +1,16 @@
-let prenom= prompt("saisir un prenom");
 let table=["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-if(table.find( element => element == prenom)){
-    let element = (table.indexOf(""))
-    console.log(table.slice(element))
-    console.log(table.push(""))
-document.write("prénom trouvé dans la liste")
-}
-else{
-    document.write("prénom non trouvé")}
+
+while(vrai = true){
+let prenom= prompt("saisir un prenom");
+let index = table.indexOf(prenom)
+if(index > -1){
+        table.splice(index,1,"")
+        
+    }
+    
+else
+{   document.write(table+"<br>")
+    document.write("Erreur"+"<br>")
+    vrai=false
+    break
+}}
