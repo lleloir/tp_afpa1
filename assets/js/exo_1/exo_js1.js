cc51c0dfd2be162053c8ced8e3ef90a8ec33d832
lleloir <leo.lelloir24@outlook.fr>
2023-01-24 13:41:56 +0100

    modif exo js

--- assets/js/exo_1/exo_js1.js
+++ assets/js/exo_1/exo_js1.js
@@ -3,22 +3,22 @@ let i = 0
 let personne1 = 0
 let personne2 = 0
 let personne3 = 0
-while (i<=100){
+while (age<100){
     let age = prompt("quel est votre âge ?")
 if (age<20){ 
     personne1++
-    i++
     continue
 }
 else if (age>20 && age<=40){
 personne2++
-i++
+continue
+}
+else if (age<=100) {
+personne3++
 continue
 }
 else {
-personne3++
-i++
-continue
+      break
 }}
 document.write("Personne de moins de 20 ans : " +personne1 + "<br>" )
 document.write("Personne dont l'âge est compris entre 20 ans et 40 ans : " +personne2 + "<br>")
