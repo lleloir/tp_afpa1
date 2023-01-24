ec09fc3e1d040bcec9c27da9bb0d856f395364b7
lleloir <leo.lelloir24@outlook.fr>
2023-01-24 14:02:46 +0100

    modif exo js

--- assets/js/exo_2/exo_js2.js
+++ assets/js/exo_2/exo_js2.js
@@ -1,12 +1,12 @@
 let tablemulti = prompt("qu'elle table de multiplication voulez-vous ?");
+document.write("Résultat de votre demande :"+"<br>");
 function table(tablemulti){
     for (i=0;i<=10;i++){
     let a =(tablemulti*i);
-    document.write("Résultat de votre demande :"+"<br>");
 document.write(tablemulti +"x"+i+"="+a+"<br>");
 }
 }
-
+table(tablemulti)
 
 
 
