ba277375e8d76cc0a0a943d0687b9a5aa29d4d42
lleloir <leo.lelloir24@outlook.fr>
2023-01-27 10:39:31 +0100

    jarditou js

---
+++ assets/js/jarditou_js.js
@@ -0,0 +1,24 @@
+let form = document.getElementById(form);
+form.addEventListener("submit",function(e){
+let nom = document.getElementById(nom);
+let prenom = document.getElementById(prenom);
+let sexe= document.getElementById(sexe);
+let date = document.getElementById(date);
+let postal = document.getElementById(postal);
+let adresse = document.getElementById(adresse);
+let ville = document.getElementById(ville);
+let sujet = document.getElementById(sujet);
+let question = document.getElementById(question);
+let données = document.getElementById(données);
+
+
+const regex1 = RegExp("^[A-Za-z]+$")
+const regex2 = RegExp("^[A-Za-z]+$"+"")
+const regex3 = RegExp("^[A-Za-z]+$")
+if(regex1.test(nom)){
+}
+else{
+    e.preventDefault()
+}
+})
+
